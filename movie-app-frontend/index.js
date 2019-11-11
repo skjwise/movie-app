bd8d9f0ab92726c327ed491eaac5e7f444e58f98
skjwise <skjwise@hotmail.com>
2019-11-11 16:04:34 +0000

    first commit

---
+++ movie-app-frontend/index.js
@@ -0,0 +1,20 @@
+//Firstly, create a function that loads the DOM content.
+//create a function that lets the user view all the movies.
+//make a baseURL for when you want to fetch
+
+//starting view contains - movie titles and images.
+
+//create an event listener that lets the user click on a movie to view all the details of the selected movie. 
+
+//create a funtion that lets a user comment on a selected movie.
+//function that lets the user update or edit their comment.
+
+//create a function that lets the user like a selected movie - make a like button
+//If you have enough time - create a function that lets the user unlike a selected movie.
+
+
+
+// document.addEventListener("DOMContentLoaded", ()=>{
+    // const baseURL = 
+
+// })
